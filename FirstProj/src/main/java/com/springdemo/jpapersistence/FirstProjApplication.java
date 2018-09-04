54f0ebd3e99beb92de51bafaf76c03625052acb9
abhinav55222 <42325981+abhinav55222@users.noreply.github.com>
2018-09-04 12:52:31 -0500

    adding comment

--- FirstProj/src/main/java/com/springdemo/jpapersistence/FirstProjApplication.java
+++ FirstProj/src/main/java/com/springdemo/jpapersistence/FirstProjApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstProjApplication {
 
 	public static void main(String[] args) {
+		
+		// start the project here
 		SpringApplication.run(FirstProjApplication.class, args);
 	}
 }
